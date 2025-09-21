e414b2089674aa3cc6959dbde5313b04ad67a940
virenrao1153-stu <virenrao1153@gmail.com>
2025-09-21 08:12:03 +0530

    opperators

---
+++ activity2.py
@@ -0,0 +1,8 @@
+Amount =int(input("Please Enter Amount for Withdraw:"))
+
+note_1 = Amount//100
+note_2 = (Amount%100)//50
+note_3 = ((Amount%100)%50)//10
+print( "notes of 100 rupee" , note_1)
+print( "notes of 50 rupee", note_2)
+print("notes of 10 rupee", note_3)
