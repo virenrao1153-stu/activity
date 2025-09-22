1e337b36c08d210f74e3a023f0766b8df3797984
virenrao1153-stu <virenrao1153@gmail.com>
2025-09-22 11:35:55 +0530

    uppercase to lowercase

---
+++ activity0.1.py
@@ -0,0 +1,3 @@
+text = input("Enter a word in lowercase: ")
+uppercase_text = text.upper()
+print("Converted word in uppercase: ", uppercase_text)
---
+++ activity7.py
@@ -0,0 +1,6 @@
+number = int(input("Enter your age"))
+if (number > 18):
+    print ("eligible to vote")
+
+else :
+    print ("not eligible to vote")
