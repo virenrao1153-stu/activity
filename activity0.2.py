75292872c3e99baac260ad60a2bd66e0a43aee91
virenrao1153-stu <virenrao1153@gmail.com>
2025-09-22 12:19:04 +0530

    birthdays

---
+++ activity0.2.py
@@ -0,0 +1,20 @@
+friend1_name = "Aman"
+friend1_birthday = "5th January"
+
+friend2_name = "Idaya"
+friend2_birthday = "12th March"
+
+friend3_name = "Yuvaan"
+friend3_birthday = "26th September"
+
+friend4_name = "Aiza"
+friend4_birthday = "25th April"
+
+friend5_name = "Aarav"
+friend5_birthday = "13 December"
+
+print(friend1_name, "birthday is off", friend1_birthday)
+print(friend2_name, "birthday is off", friend2_birthday)
+print(friend3_name, "birthday is on", friend3_birthday)
+print(friend4_name, "birthday is off", friend4_birthday)
+print(friend5_name, "birthday is on",friend5_birthday)
