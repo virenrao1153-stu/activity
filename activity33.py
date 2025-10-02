ebfeb482e87b3a61378035f0f43b8f4c488f701c
virenrao1153-stu <virenrao1153@gmail.com>
2025-10-02 19:50:22 +0530

    Mean value

---
+++ activity33.py
@@ -0,0 +1,10 @@
+print("Enter Number 1")
+x = int(input())
+print("Enter Number 2")
+y = int(input())
+print("Enter Number 3")
+z = int(input())
+
+n = x + y + z
+k = n / 3
+print("The mean of three numbers is ", k)
