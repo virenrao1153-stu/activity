3197b504168875002058d33b28e219e65e88b506
virenrao1153-stu <virenrao1153@gmail.com>
2025-10-02 19:35:06 +0530

    Divisible number

---
+++ activity32.py
@@ -0,0 +1,9 @@
+print("Enter a Number (Numerator):")
+numn = int(input())
+print("Enter a Number (denominator):")
+numd = int(input())
+
+if numn%numd==0:
+    print("Number is divisible")
+else:
+    print("Number is not divisible")
