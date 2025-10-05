baa67f829b10597be0bb796481110935b4f83e20
virenrao1153-stu <virenrao1153@gmail.com>
2025-10-05 19:38:46 +0530

    String reverse

---
+++ activity43.py
@@ -0,0 +1,8 @@
+string = input("Please enter your own string : ")
+
+string2 = ('')
+for i in string:
+    string2 = i + string2
+
+print("\nThe Orignal String = ", string)
+print("The Reversed String = ", string2)
