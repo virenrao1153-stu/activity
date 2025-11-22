9102b322876ec0c0a575b43d61fe434cd86ad112
virenrao1153-stu <virenrao1153@gmail.com>
2025-11-22 15:15:11 +0530

    symmetric difference

---
+++ activity107.py
@@ -0,0 +1,20 @@
+set1_A = {"blue", "green"}
+set2_A = {"blue", "yellow"}
+
+sym_diff_A = set1_A.symmetric_difference(set2_A)
+
+print("Set A:")
+print("Set 1 =", set1_A)
+print("Set 2 =", set2_A)
+print("Symmetric Difference =", sym_diff_A)
+print("--------------------------------------------")
+
+set1_B = {1, 2, 3, 4, 5}
+set2_B = {1, 5, 6, 7, 8, 9}
+
+sym_diff_B = set1_B.symmetric_difference(set2_B)
+
+print("Set B:")
+print("Set 1 =", set1_B)
+print("Set 2 =", set2_B)
+print("Symmetric Difference =", sym_diff_B)
