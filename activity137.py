bf4f137e33981d341d91bd8f50ad4397bc86772f
virenrao1153-stu <virenrao1153@gmail.com>
2025-12-28 19:24:14 +0530

    Virus detected

---
+++ activity137.py
@@ -0,0 +1,11 @@
+from tkinter import *
+from tkinter import messagebox
+root = Tk()
+root.geometry("200x200")
+def msg():
+    messagebox.showwarning("Alert", "Stop! Virus Found.")
+
+button = Button(root, text="Scan for Virus", command=msg)
+button.place(x=40, y=80)
+
+root.mainloop()
