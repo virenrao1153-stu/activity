866f92a0e2a34e3cde18887bc57f9c630abf840f
virenrao1153-stu <virenrao1153@gmail.com>
2026-01-24 19:34:55 +0530

    digit predictor

---
+++ activity162.py
@@ -0,0 +1,39 @@
+import tensorflow as tf
+from tensorflow.keras import layers, models
+import matplotlib.pyplot as plt
+
+# Load dataset
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+
+# Normalize
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+# Model
+model = models.Sequential([
+    layers.Flatten(input_shape=(28, 28)),
+    layers.Dense(128, activation='relu'),
+    layers.Dense(10, activation='softmax')
+])
+
+# Compile
+model.compile(
+    optimizer='adam',
+    loss='sparse_categorical_crossentropy',
+    metrics=['accuracy']
+)
+
+# Train
+model.fit(x_train, y_train, epochs=5)
+
+# Evaluate
+test_loss, test_acc = model.evaluate(x_test, y_test)
+print(f"Test accuracy: {test_acc}")
+
+# Predict
+prediction = model.predict(x_test)
+
+# Show first test image
+plt.imshow(x_test[0], cmap=plt.cm.binary)
+plt.title(f"Predicted: {prediction[0].argmax()}")
+plt.axis('off')
+plt.show()
